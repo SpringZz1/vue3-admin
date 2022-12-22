b4a615f34ae9ca1f7bfa1d6fca9ba1ebaaad6757
[200~SpringZz1~ <wang90944@gmail.com>
2022-12-22 14:45:48 +0800

    添加关闭名称验证

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,18 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-prettier'
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-prettier",
   ],
+  rules: {
+    // 关闭名称验证
+    "vue/multi-word-component-names": "off",
+  },
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: "latest",
+  },
+};
