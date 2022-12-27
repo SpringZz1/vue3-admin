8e84d99ef07bd9848da375662ec49f43a9ccbe3c
SpringZz <wang90944@gmail.com>
2022-12-27 23:52:35 +0800

    底部footer组件完成

--- src/components/common/AppFooter.vue
+++ src/components/common/AppFooter.vue
@@ -1,7 +1,23 @@
 <template>
-  <div>我是footer</div>
+  <footer class="info">
+    <p>
+      Copyright©2023
+      <a href="https://github.com/SpringZz1" target="__blank">SpringZz</a>
+    </p>
+  </footer>
 </template>
 
 <script setup></script>
 
-<style lang="sass" scoped></style>
+<style lang="scss" scoped>
+.info {
+  position: fixed;
+  bottom: 40px;
+  p {
+    color: #6a6a6a;
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+}
+</style>
